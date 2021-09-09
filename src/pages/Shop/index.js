105fea33d853a90bdcf17ffe4070b1b67a3f29bf
leonmjuancarlos <leonmjuancarlos@outlook.com>
2021-09-09 20:13:42 +0200

    feat: add pagination scroll up

--- src/pages/Shop/index.js
+++ src/pages/Shop/index.js
@@ -18,6 +18,7 @@ export default function Shop() {
           type: 'CHANGE_PAGE',
           page: Number.parseInt(e.target.textContent),
         })
+      window.scrollTo(0, 0)
     },
     [state.page]
   )
